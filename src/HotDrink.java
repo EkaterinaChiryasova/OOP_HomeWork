4be4c2c4c1ef1c426c26b7988933c2ae3d3901dd
Ekaterina Sazhina <teos2910@mail.ru>
2024-02-13 18:37:32 +0500

    add hometask

---
+++ src/HotDrink.java
@@ -0,0 +1,27 @@
+public class HotDrink extends BottleOfWater {
+    private int temperature;
+
+    public HotDrink(String name, int cost, double volume, int temperature) {
+        super(name, cost, volume);
+        this.temperature = temperature;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    @Override
+    public String toString() {
+        return "HotDrink{" +
+                "name=" + super.getName() +'\'' +
+                ", temperature=" + temperature +
+                ", volume=" + super.getVolume() +
+                ", cost=" + super.getCost() +
+
+                '}';
+    }
+}
---
+++ src/VendingMachine.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+ public interface VendingMachine {
+     void initProducts (List <Product> list);
+    public Product getProduct(String name);
+
+
+
+
+}
